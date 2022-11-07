68c2040b0c3ed47dfef622dbeeaf65c362cf0c89
K-Mehri-2022 <117268051+K-Mehri-2022@users.noreply.github.com>
2022-11-07 08:53:23 -0800

    Update P42.cpp

--- P42.cpp
+++ P42.cpp
@@ -5,10 +5,10 @@ using namespace std;
 main() {
 	
 int x,y,z;
-float s;
+float w;
 	
 cin >> x >> y >> z;
-s=(float)((x-y)*(x+z))/((x+y+z)*(x+y+z)+1);
-cout << s;
+w=(float)((x-y)*(x+z))/((x+y+z)*(x+y+z)+1);
+cout << w;
 
 }
