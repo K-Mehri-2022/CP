25adbac02382a2262021cd7caba6791d6d8e7738
K-Mehri-2022 <117268051+K-Mehri-2022@users.noreply.github.com>
2022-11-10 06:11:30 -0800

    Add files via upload

---
+++ P52.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+char op;
+cout << " please enter an english letter : ";
+cin >> op;
+
+switch (op){
+	case 'a':
+		cout << "vowel";
+		break;
+	case 'e':
+		cout << "vowel";
+        break;
+    case 'i':
+    	cout << "vowel";
+    	break;
+    case 'o':
+    	cout << "vowel";
+    	break;
+    case 'u':
+    	cout << "vowel";
+    }
+}
